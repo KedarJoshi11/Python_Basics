c35e2ab57e4af89d1ab0a8d23adbcea2e1537dc4
Kedar Joshi <43803843+KedarJoshi11@users.noreply.github.com>
2020-09-14 00:11:45 +0530

    Add files via upload

---
+++ selection_sort.py
@@ -0,0 +1,33 @@
+import sys
+#function to capture list
+def capture_array():
+	print("Enter \"done\" when list is compelete. ")
+	l1 = []
+	while True:
+		a = input("Enter element: ")
+		if a == "Done" or a == "done" or a == "DONE":
+			break
+		l1.append(a)
+
+	return l1
+#function for sorting the list
+def selectionSort(array, size):
+	for step in range(size):
+		min_idx = step
+
+		for i in range(step + 1, size):
+			#to sort in descending order, change > to < in this line
+			#select the minimum element in each loop
+			if array[i] < array[min_idx]:
+				min_idx = i
+
+		(array[step], array[min_idx]) = (array[min_idx], array[step])
+
+data = capture_array()
+print("The array you entered is: ")
+print(data)
+size = len(data)
+selectionSort(data, size)
+print("After performing selection sort on the array. ")
+print(data)
+
